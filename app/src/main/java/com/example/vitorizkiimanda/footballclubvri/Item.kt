9b74310c5b3864057728b48966080bab8706ef5d
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-10-19 16:06:45 +0700

    done : recylceView, detail page, android extension, anko common

--- app/src/main/java/com/example/vitorizkiimanda/footballclubvri/Item.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballclubvri/Item.kt
@@ -1,3 +1,32 @@
 package com.example.vitorizkiimanda.footballclubvri
 
-data class Item (val name: String?, val image: Int?)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Item (val name: String?, val description: String?, val image: Int?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readValue(Int::class.java.classLoader) as? Int) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeValue(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Item> {
+        override fun createFromParcel(parcel: Parcel): Item {
+            return Item(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Item?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/vitorizkiimanda/footballclubvri/MainActivity.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballclubvri/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.vitorizkiimanda.footballclubvri
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.Gravity
+import android.widget.LinearLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,27 +18,72 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        MainActivityUI().setContentView(this)
         setContentView(R.layout.activity_main)
+
+        val mProgressDialog = indeterminateProgressDialog("Loading data..")
+        mProgressDialog.show()
 
         initData()
 
+        Handler().postDelayed(
+                {
+                    // This method will be executed once the timer is over
+                    mProgressDialog.dismiss()
+                },
+                2000 // value in milliseconds
+        )
+
         club_list.layoutManager = LinearLayoutManager(this)
         club_list.adapter = RecyclerViewAdapter(this, items){
-            val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
-            toast.show()
+            startActivity<ClubDetailActivity>("data" to it)
         }
     }
 
     private fun initData(){
+
         val name = resources.getStringArray(R.array.club_name)
         val image = resources.obtainTypedArray(R.array.club_image)
+        val description = resources.getStringArray(R.array.club_description)
         items.clear()
         for (i in name.indices) {
-            items.add(Item(name[i],
-                    image.getResourceId(i, 0)))
+            items.add(
+                    Item(
+                            name[i],
+                            description[i],
+                            image.getResourceId(i, 0)
+                    )
+            )
         }
 
         //Recycle the typed array
         image.recycle()
     }
+
+//    class MainActivityUI : AnkoComponent<MainActivity> {
+//        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+//            linearLayout {
+//                orientation = LinearLayout.HORIZONTAL
+//                padding = dip(16)
+//
+//                imageView(R.drawable.img_barca) {
+//                    id = Ids.image
+//                }.lparams(width = dip(50), height = dip(50))
+//
+//                textView {
+//                    id = Ids.name
+//                    text = "Barcelona FC"
+//                }.lparams(width = wrapContent, height = wrapContent) {
+//                    margin = dip(10)
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    private object Ids {
+//        val image = 1
+//        val name = 2
+//    }
 }
